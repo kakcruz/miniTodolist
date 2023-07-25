2c850e63f0033a2830a537c26dc063ea2218350a
Kakcruz <karolinecsantos@hotmail.com>
2023-07-25 15:34:55 -0300

    Aula4 -check

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,67 +1,107 @@
 <template>
-    <div class="bg-gray-300 rounded-sm">
-          <div
-            class="flex items-center px-4 py-3 border-b border-gray-400 last:border-b-0"
+  <div class="bg-gray-300 rounded-sm">
+    <div
+      class="flex items-center px-4 py-3 border-b border-gray-400 last:border-b-0"
+    >
+      <div class="flex items-center justify-center mr-2">
+        <button 
+        :class="{
+          'text-red-400': isCompleted,
+          'text-grenn-600': !isCompleted,
+        }"
+        @click="onCheckClick"
+        >
+          <svg
+            class="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            <div class="flex items-center justify-center mr-2">
-              <button class="text-gray-400">
-                <svg
-                  class="w-5 h-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M5 13l4 4L19 7"
-                  ></path>
-                </svg>
-              </button>
-            </div>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 13l4 4L19 7"
+            ></path>
+          </svg>
+        </button>
+      </div>
 
-            <div class="w-full">
-              <input
-                type="text"
-                placeholder="Digite a sua tarefa"
-                :value="todo.title"
-                class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3"
-              />
-            </div>
+      <div class="w-full">
+        <input
+          v-model="title"
+          placeholder="Digite a sua tarefa"
+          class="bg-gray-300 placeholder-gray-500 text-gray-700 font-light focus:outline-none block w-full appearance-none leading-normal mr-3"
+          @keyup.enter="onTitleChange"
+        />
+        <!--  modificador enter colocado, para quando apenas disparar a tecla enter -->
+      </div>
 
-            <div class="ml-auto flex items-center justify-center">
-              <button class="focus:outline-none">
-                <svg
-                  class="ml-3 h-4 w-4 text-gray-500"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19 7L18.1327 19.1425C18.0579 
+      <div class="ml-auto flex items-center justify-center">
+        <button class="focus:outline-none">
+          <svg
+            class="ml-3 h-4 w-4 text-gray-500"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19 7L18.1327 19.1425C18.0579 
 20.1891 17.187 21 16.1378 21H7.86224C6.81296 21 5.94208 20.1891 5.86732 
 19.1425L5 7M10 11V17M14 11V17M15 7V4C15 3.44772 14.5523 3 14 3H10C9.44772 
 3 9 3.44772 9 4V7M4 7H20"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   props: {
-    todo:{
+    todo: {
       type: Object,
       default: () => ({}),
+    },
+  },
+  data(){
+    return{
+      isCompleted: this.todo.completed,
+      title: this.todo.title,
     }
-  }
-}
+  },
+ 
+  methods: {
+    onTitleChange() {
+      if(!this.title){
+        return {
+
+        }
+      }
+      this.updateTodo()
+    },
+
+    updateTodo(){
+      const payload = {
+        id: this.todo.id,
+        data: {
+          title: this.title,
+          completed: this.todo.completed
+        }
+      }
+      this.$store.dispatch('updateTodo' , payload)
+    },
+
+    onCheckClick(){
+      this.isCompleted = !this.isCompleted
+      this,this.updateTodo()
+    }
+  },
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -28,9 +28,11 @@ export default createStore({
     addTodo({commit}, data){
        return axios.post('http://localhost:3000/todos', data).then((response) =>{
         commit('storeTodo', response.data);
-
       })
-    }
+    },
+    updateTodo(context,{ id, data }){
+      return axios.put(`http://localhost:3000/todos/${id}`, data)
+   }
   },
   getters: {},
 
